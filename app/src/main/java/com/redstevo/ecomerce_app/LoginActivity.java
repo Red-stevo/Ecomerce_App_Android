fed664daf4761920ed278aab851f9076586371fe
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-12-25 07:41:08 +0300

    Commit message >>>>> added the on login event listener. <<<<<

--- app/src/main/java/com/redstevo/ecomerce_app/LoginActivity.java
+++ app/src/main/java/com/redstevo/ecomerce_app/LoginActivity.java
@@ -2,7 +2,10 @@ package com.redstevo.ecomerce_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,13 +19,25 @@ public class LoginActivity extends AppCompatActivity {
 
 
         /*Handle moving to the registration page.*/
-        TextView textView = findViewById(R.id.register_link);
-        textView.setOnClickListener(view -> {
+        TextView registrationPageLink = findViewById(R.id.register_link);
+        registrationPageLink.setOnClickListener(view -> {
             Intent registerIntent = new Intent(LoginActivity.this, RegistrationActivity.class);
             startActivity(registerIntent);
         });
 
         /*Handle the login click*/
+        Button loginButton = findViewById(R.id.login_btn);
+
+        loginButton.setOnClickListener(view -> {
+
+            /*username input login page*/
+            EditText usernameInput = findViewById(R.id.username_input);
+            Toast.makeText(LoginActivity.this, usernameInput.getText(), Toast.LENGTH_LONG).show();
+
+            /*password input registration page.*/
+            EditText loginPasswordInput = findViewById(R.id.password_input);
+            Toast.makeText(LoginActivity.this,loginPasswordInput.getText(), Toast.LENGTH_LONG).show();
+        });
 
     }
 }
