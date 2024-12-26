a1b27373179f2ba5fb16150c6f7c44551e0046e4
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-12-26 06:48:15 +0300

    Commit message >>>>> handling image and video upload <<<<<

--- app/src/main/java/com/redstevo/ecomerce_app/Activities/AddProductActivity.java
+++ app/src/main/java/com/redstevo/ecomerce_app/Activities/AddProductActivity.java
@@ -1,8 +1,12 @@
 package com.redstevo.ecomerce_app.Activities;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,6 +17,7 @@ import com.redstevo.ecomerce_app.R;
 
 public class AddProductActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +26,18 @@ public class AddProductActivity extends AppCompatActivity {
         /* Handle addition of video or image */
         FloatingActionButton addImageVideo = findViewById(R.id.handleAddImageVideo);
         addImageVideo.setOnClickListener(event -> {
-
+            Intent uploadIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            uploadIntent.setType("image/** | video/**");
+            uploadIntent.putExtra(Intent.EXTRA_QUIET_MODE, Activity.RESULT_OK);
+            startActivityForResult(uploadIntent, RESULT_OK);
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+    }
+
 }
