03061585b674992df2e9232c553f79c619833a1f
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-12-27 07:20:28 +0300

    Commit message >>>>> image vidoe preview model <<<<<

---
+++ app/src/main/java/Models/ImagePreviewModel.java
@@ -0,0 +1,25 @@
+package Models;
+
+import android.net.Uri;
+
+public class ImagePreviewModel {
+    private String imageVideoUrl;
+
+    private Uri imageVideoUri;
+
+    public String getImageVideoUrl() {
+        return imageVideoUrl;
+    }
+
+    public void setImageVideoUrl(String imageVideoUrl) {
+        this.imageVideoUrl = imageVideoUrl;
+    }
+
+    public Uri getImageVideoUri() {
+        return imageVideoUri;
+    }
+
+    public void setImageVideoUri(Uri imageVideoUri) {
+        this.imageVideoUri = imageVideoUri;
+    }
+}
