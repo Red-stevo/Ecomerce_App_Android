26169c10152199e64dd707c32ad663f0b4e40edc
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-12-31 07:24:05 +0300

    Commit message >>>>> working on the general activity view <<<<<

---
+++ app/src/main/java/com/redstevo/ecomerce_app/Activities/GeneralActivity.java
@@ -0,0 +1,26 @@
+package com.redstevo.ecomerce_app.Activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.redstevo.ecomerce_app.R;
+
+public class GeneralActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_general);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
