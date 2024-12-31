501ccd6e14a9c941a35b4f68c4e4bd64d125dd60
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-12-31 18:42:49 +0300

    Commit message >>>>> added the on enter event listener <<<<<

--- app/src/main/java/com/redstevo/ecomerce_app/Activities/GeneralView/GeneralActivity.java
+++ app/src/main/java/com/redstevo/ecomerce_app/Activities/GeneralView/GeneralActivity.java
@@ -1,6 +1,10 @@
 package com.redstevo.ecomerce_app.Activities.GeneralView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -10,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.redstevo.ecomerce_app.R;
+
+import java.util.Objects;
 
 public class GeneralActivity extends AppCompatActivity {
 
@@ -36,6 +42,15 @@ public class GeneralActivity extends AppCompatActivity {
         ImageView trackOrder = findViewById(R.id.track_order);
         trackOrder.setOnClickListener(event -> {
 
+        });
+
+        EditText editText = findViewById(R.id.search_product);
+        editText.setOnKeyListener((v, keyCode, event) -> {
+           if(event.getAction() == KeyEvent.ACTION_DOWN){
+                String searchQuery = editText.getText().toString();
+               Log.d("SEARCH_BAR", "onKey: "+searchQuery);
+            }
+            return true;
         });
     }
 }
