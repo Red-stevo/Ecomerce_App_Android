c160f68d81f8ff14714d5ea8a060ccf1f0fbd42e
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2025-01-04 11:57:07 +0300

    Commit message >>>>> working on single produc view <<<<<

---
+++ app/src/main/java/com/redstevo/ecomerce_app/Activities/SingleProduct/singleProductViewActivity.java
@@ -0,0 +1,17 @@
+package com.redstevo.ecomerce_app.Activities.SingleProduct;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.redstevo.ecomerce_app.R;
+
+public class singleProductViewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_product_view);
+
+    }
+}
