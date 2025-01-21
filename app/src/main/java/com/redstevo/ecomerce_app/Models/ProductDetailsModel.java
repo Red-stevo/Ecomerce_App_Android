38e36532cc0ddb0dc34b666c2f568b7d0e4b44ae
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2025-01-21 16:33:39 +0300

    Commit message >>>>> working on single product vew <<<<<

---
+++ app/src/main/java/com/redstevo/ecomerce_app/Models/ProductDetailsModel.java
@@ -0,0 +1,27 @@
+package com.redstevo.ecomerce_app.Models;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ProductDetailsModel {
+
+    private String productName;
+
+    private String productDescription;
+
+    private Integer productCount;
+
+    private Float productPrice;
+
+    private  Float productDiscount;
+
+    private List<String> productImagesUrls;
+
+    public ProductDetailsModel() {
+
+    }
+}
--- app/src/main/java/com/redstevo/ecomerce_app/Services/GetProduct.java
+++ app/src/main/java/com/redstevo/ecomerce_app/Services/GetProduct.java
@@ -1,10 +1,12 @@
 package com.redstevo.ecomerce_app.Services;
 
+import com.redstevo.ecomerce_app.Models.ProductDetailsModel;
+
 public class GetProduct {
 
-    public void getProductByKey(String key){
+    public ProductDetailsModel getProductByKey(String key){
         key = "/-OH7MPdJHh3kwdI8YWbO"; ///-OH7M3reF6qlMca1lVB-
 
-
+        return new ProductDetailsModel();
     }
 }
