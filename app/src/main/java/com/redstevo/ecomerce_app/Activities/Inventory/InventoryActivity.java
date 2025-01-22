356e9b2cf506cb665bd28d50dddc140f6ebd024f
mirowe <mirowe@localhost.localdomain>
2025-01-22 14:30:50 +0300

    ...

--- app/src/main/java/com/redstevo/ecomerce_app/Activities/Inventory/InventoryActivity.java
+++ app/src/main/java/com/redstevo/ecomerce_app/Activities/Inventory/InventoryActivity.java
@@ -1,18 +1,13 @@
 package com.redstevo.ecomerce_app.Activities.Inventory;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.redstevo.ecomerce_app.Activities.GeneralView.GeneralActivity;
 import com.redstevo.ecomerce_app.Adapters.InventoryAdapter;
 import com.redstevo.ecomerce_app.Models.InventoryItem;
@@ -21,16 +16,12 @@ import com.redstevo.ecomerce_app.R;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class InventoryActivity extends GeneralActivity {
 
     DatabaseReference databaseReference;
-    private InventoryAdapter inventoryAdapter;
-    private List<InventoryItem> inventoryItem;
 
 
 
-    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -39,39 +30,21 @@ public class InventoryActivity extends GeneralActivity {
 
 
 
-        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance("https://myapplication-fce0cb20-default-rtdb.firebaseio.com/");
-        databaseReference = firebaseDatabase.getReference("cartAAA");
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("products");
 
         RecyclerView recyclerView = findViewById(R.id.rvInventory);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        inventoryItem = new ArrayList<>();
-        inventoryAdapter = new InventoryAdapter(inventoryItem);
+        List<InventoryItem> inventoryItem = new ArrayList<>();
+        InventoryAdapter inventoryAdapter = new InventoryAdapter(inventoryItem);
         recyclerView.setAdapter(inventoryAdapter);
 
-        Log.d("CHECK", "Checking reference path: " + databaseReference.getPath());
+        Log.d("CHECK", "Checking reference path: " + databaseReference.toString());
         Log.d("CHECK","checking if data is present in "+ databaseReference.getKey());
 
 
 
 
-        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Log.d("DATA", "the data is "+snapshot.getChildren());
-                Log.d("INVENTORY"," "+snapshot);
-                for (DataSnapshot snapshot1 : snapshot.getChildren()){
-
-
-
-                }
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
     }
 }
